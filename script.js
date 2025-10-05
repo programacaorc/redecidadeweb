ba5ba7bf55931ab9ed05178c94dead6768fa282f
Diretoria Cidade WEB <canalbicabicudo13@gmail.com>
2025-10-05 17:37:44 -0300

    Update script.js

--- script.js
+++ script.js
@@ -94,7 +94,7 @@ const placares = [
         gols_casa: 3,
         time_fora: "Vitória",
         escudo_fora: "https://upload.wikimedia.org/wikipedia/pt/thumb/3/34/Esporte_Clube_Vit%C3%B3ria_logo.png/120px-Esporte_Clube_Vit%C3%B3ria_logo.png",
-        gols_fora: 2
+        gols_fora: 3
     },
     {
         campeonato: "Brasileirão Série A",
@@ -106,7 +106,7 @@ const placares = [
         gols_casa: 2,
         time_fora: "Palmeiras",
         escudo_fora: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Palmeiras_logo.svg/250px-Palmeiras_logo.svg.png",
-        gols_fora: 1
+        gols_fora: 2
     },
     {
         campeonato: "Brasileirão Série A",
