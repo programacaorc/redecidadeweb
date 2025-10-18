990f153c07e641f5059e9aa243d785b180e044c3
Diretoria Cidade WEB <canalbicabicudo13@gmail.com>
2025-10-18 20:19:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -56,7 +56,7 @@ const placares = [
         hora: "18:30",
         time_casa: "Corinthians",
         escudo_casa: "https://upload.wikimedia.org/wikipedia/pt/thumb/b/b4/Corinthians_simbolo.png/250px-Corinthians_simbolo.png",
-        gols_casa: 0,
+        gols_casa: 1,
         time_fora: "Atlético",
         escudo_fora: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Atletico_mineiro_galo.png/250px-Atletico_mineiro_galo.png",
         gols_fora: 0
