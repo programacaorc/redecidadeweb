ec5b8fd65a9f549ab9aaf9c6255da66f5937f5da
Diretoria Cidade WEB <canalbicabicudo13@gmail.com>
2025-11-01 22:58:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -128,7 +128,7 @@ const placares = [
     },
     {
         campeonato: "Brasileirão Série A",
-        status: "AO VIVO",
+        status: "ENCERRADO",
         data: "01/11",
         hora: "21:00",
         time_casa: "Flamengo",
